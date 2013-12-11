65b7bb8dd2f95a2b753012b7d614cd5723790a2a
2hdddg <2hdddg@gmail.com>
2013-12-11 22:00:24 +0100

    Siren to html

--- test/unit/siren/siren_test.js
+++ test/unit/siren/siren_test.js
@@ -10,28 +10,32 @@ describe('Siren,', function(){
             assert.equal(html, '<article></article>');
         });
 
-        it('should set class on article tag if present', function(){
+        it('should wrap entity title in ht tag', function(){
+            var html = siren.toHtml({
+                title: 'Atitle'
+            });
+            assert.equal(html, '<article><h1>Atitle</h1></article>');
+        });
+
+        it('should set entity class as class on article tag if present', function(){
             var html = siren.toHtml({
                 class: ["aclass"]
             });
             assert.equal(html, '<article class="aclass"></article>');
         });
 
-        it('should set classes on article tag if more than one', function(){
+        it('should set entity classes on article tag if more than one', function(){
             var html = siren.toHtml({
                 class: ["aclass", "anotherclass" ]
             });
             assert.equal(html, '<article class="aclass anotherclass"></article>');
         });
 
-        it('transforms properties into dl list', function(){
+        it('should set entity classes on article tag if more than one', function(){
             var html = siren.toHtml({
-                properties: {
-                    "orderNumber": 42,
-                    "status": "pending"
-                }
+                class: ["aclass", "anotherclass" ]
             });
-            assert.equal(html, '<article><dl><dt>orderNumber</dt><dd>42</dd><dt>status</dt><dd>pending</dd></dl></article>');
+            assert.equal(html, '<article class="aclass anotherclass"></article>');
         });
 
         it('transforms entities into ul list of sections', function(){
